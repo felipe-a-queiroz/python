7973e900bdfafb12e00424827c66faf9c89127c5
Felipe Queiroz <felipe.de.a.queiroz@outlook.com>
2022-01-15 16:02:34 -0300

    Escopo
    Signed-off-by: Felipe Queiroz <felipe.de.a.queiroz@outlook.com>

--- escopo.py
+++ escopo.py
@@ -1,3 +1,14 @@
-numero = '42'
-print(numero)
-print(type(numero))
+num = 42
+
+print(num)
+
+print(type(num))
+
+novo = 0
+print(novo)
+
+if num > 10:
+    novo = num + 10
+    print(novo)
+
+print(novo)
