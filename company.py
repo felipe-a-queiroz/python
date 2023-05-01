45dab01d3ef272ead91031c8fdb6c8d12e3b1a97
Felipe Queiroz <felipe.de.a.queiroz@accenture.com>
2023-05-01 14:20:04 -0300

    Using Classes and Defining Methods

---
+++ company.py
@@ -0,0 +1,39 @@
+from employee import Employee
+
+class Company:
+    def __init__(self):
+        self.employees = []
+    
+    def add_employee(self, new_employee):
+        self.employees.append(new_employee)
+
+    def display_employees(self):
+        print('Current employees:')
+        for employee in self.employees:
+            print(employee.fname, employee.lname)
+        print('----------')
+    
+    def pay_employees(self):
+        print('Paying employees:')
+        for employee in self.employees:
+            print('Paying check for:', employee.fname, employee.lname)
+            print(f'Amount: ${employee.calculate_paycheck():,.2f}')
+        print('----------')
+
+
+def main():
+    my_company = Company()
+
+    employee1 = Employee('Sarah', 'Hess', 50000)
+    my_company.add_employee(employee1)
+
+    employee2 = Employee('Lee', 'Smith', 25000)
+    my_company.add_employee(employee2)
+
+    employee3 = Employee('Bob', 'Brown', 60000)
+    my_company.add_employee(employee3)
+
+    my_company.display_employees()
+    my_company.pay_employees()
+
+main()
